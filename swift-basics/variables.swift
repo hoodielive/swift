800c10aee43c60956e249e4fd98369038697c9cc
hoodielive <hoodielive@gmail.com>
2019-01-15 17:48:40 -0500

    Boredom killed this session lol

---
+++ swift-basics/variables.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where peoples can play 
+
+import UIKit
+
+// variables
+var message = "Hello, playground"
+
+// Operators: Unary, Binary, Ternary
+
+var amICool = true 
+
+amICool = !amICool // Unary Operator = false
+
+var feelGoodAbout = true
+
+feelGoodAbout = amICool ? true : false 
+
